e3fb23751319e9482c49595f99ec7574c9f91ae7
Javiermd <javiermardom@gmail.com>
2023-10-03 12:44:01 +0200

    scripts

---
+++ Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    // Esto es una prueba de comentario
+    private int age =  26;
+    private float price = 6.2f;
+    private bool isGameOver = false;
+    private string name = "Javier Martinez Dominguez";
+
+}
